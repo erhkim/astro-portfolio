cf93fb2cc8f6a4f8299e5def9e874b270127e0a8
Eric Kim <erhkim@protonmail.com>
2025-03-03 00:09:12 -0800

    Add github pages config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://erhkim.github.io',
+    base: 'astro-portfolio',
+  })
