1bb43360606d20bdf458c4c1c109e15ed1fdbdf1
Eric Kim <erhkim@protonmail.com>
2025-03-10 23:00:32 -0700

    Revert GitHub pages change and add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-    site: 'https://erhkim.github.io',
-    base: '/astro-portfolio',
-  })
+export default defineConfig({})
